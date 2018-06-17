60bedaaacf17f0b642a25c2cb12641f44828dab3
日高彩伽 <hidakaayaka@hidakaayakanoMacBook-ea.local>
2018-06-17 22:30:16 +0900

    ユーザーと記事を紐付け

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,4 +1,8 @@
 class Article < ApplicationRecord
+    belongs_to :user
+    #投稿を新着順に並べる
+    # default_scope -> order{(created_at: :desc)}
+    validates :user_id, presence: true
     validates :title, presence: true, length:{maximum: 50}
     validates :content, presence: true, length:{maximum: 1000}
 end
