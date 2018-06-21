327f46d1b98178835f3f9b93b46797f89739785e
日高彩伽 <hidakaayaka@hidakaayakanoMacBook-ea.local>
2018-06-21 23:07:49 +0900

    ユーザーモデルのバリデーションのテスト

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,12 +1,5 @@
 require 'rails_helper'
-# バリデーション
-# 空欄
 describe User do
-  # context '何も入力しなかった場合エラーが4つ出る' do
-  #   subject { User.create }
-  #   it { should_not be_valid }
-  #   it { expect(subject.errors.size).to eq 4 }
-  # end
   subject { User.new(
     name: "Tarou Yamada",
     email: "tarou@test.com",
@@ -28,7 +21,7 @@ describe User do
       subject.save
     end
     it { should_not be_valid }
-    it { expect(subject.errors.size).to eq 1 }
+    it { expect(subject.errors.full_messages[0]).to eq "名前を入力してください" }
   end
 
   context '名前が50文字を超えるとエラーになること' do
@@ -37,6 +30,63 @@ describe User do
       subject.save
     end
     it { should_not be_valid }
-    it { expect(subject.errors.size).to eq 1 }
+    it { expect(subject.errors.full_messages[0]).to eq "名前は50文字以内で入力してください" }
+  end
+
+  context 'メールアドレスが空欄のときエラーになること' do
+    before do
+      subject.name = "T" * 50
+      subject.email = ""
+      subject.save
+    end
+    it { should_not be_valid }
+    it { expect(subject.errors.full_messages[0]).to eq "メールアドレスを入力してください" }
+  end
+
+  context 'メールアドレスが255文字を超えるとエラーになること' do
+    before do
+      subject.email = "t" * 255
+      subject.save
+    end
+    it { should_not be_valid }
+    it { expect(subject.errors.full_messages[0]).to eq "メールアドレスは255文字以内で入力してください" }
+  end
+
+  context 'メールアドレスの形式が不正なときエラーになること' do
+    before do
+      subject.email = "testtest.com"
+      subject.save
+    end
+    it { should_not be_valid }
+    it { expect(subject.errors.full_messages[0]).to eq "メールアドレスは不正な値です" }
+  end
+
+  context 'メールアドレスに半角英数記号以外を含むときエラーになること' do
+    before do
+      subject.email = "テスト@test.com"
+      subject.save
+    end
+    it { should_not be_valid }
+    it { expect(subject.errors.full_messages[0]).to eq "メールアドレスは不正な値です" }
+  end
+
+  context '登録ずみのメールアドレスはエラーになること' do
+    before do
+      subject.email = "tarou@test.com"
+      subject.save
+    end
+    it { should_not be_valid }
+    it { expect(subject.errors.full_messages[0]).to eq "メールアドレスはすでに存在します" }
+  end
+
+  context 'パスワードが空欄の時エラーになること' do
+    before do
+      subject.email = "tarou@test.jp"
+      subject.password = ""
+      subject.password_confirmation = ""
+      subject.save
+    end
+    it { should_not be_valid }
+    it { expect(subject.errors.full_messages[0]).to eq "パスワード（確認）とパスワードの入力が一致しません" }
   end
 end
