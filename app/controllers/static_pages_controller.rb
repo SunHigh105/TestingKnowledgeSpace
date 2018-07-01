741ed10a335ba9995cb1dc8419d3ca3eb332e148
日高彩伽 <hidakaayaka@hidakaayakanoMacBook-ea.local>
2018-07-01 15:04:45 +0900

    フォームの改修

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,5 +1,5 @@
 class StaticPagesController < ApplicationController
   def home
-    puts @latest_articles = Article.order("created_at DESC").find(Array(1..3))
+    @latest_articles = Article.order("created_at DESC")
   end
 end
